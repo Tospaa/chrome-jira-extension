d8ea191ca55e6ec67ac856c0885106885888c3e7
Tospaa <musaecer@gmail.com>
2022-01-05 15:56:50 +0300

    added list functional areas
    for trilogy jira

--- jiras/trilogy.js
+++ jiras/trilogy.js
@@ -4,9 +4,11 @@ class TrilogyJira {
   static contextMenuId = 'trilogy';
   static openSelectedContextMenuId = 'trilogy-jira-open-selected';
   static findTicketGivenLegacyContextMenuId = 'trilogy-find-ticket-given-legacy';
+  static listFunctionalAreasContextMenuId = 'trilogy-list-functional-areas';
   static functionMap = {
     [TrilogyJira.openSelectedContextMenuId]: TrilogyJira.openSelected,
     [TrilogyJira.findTicketGivenLegacyContextMenuId]: TrilogyJira.findTicketGivenLegacy,
+    [TrilogyJira.listFunctionalAreasContextMenuId]: TrilogyJira.listFunctionalAreas,
   };
 
   static createContextMenu() {
@@ -30,6 +32,13 @@ class TrilogyJira {
       parentId: TrilogyJira.contextMenuId,
       ...Handler.defaultContextMenuProps,
     });
+
+    chrome.contextMenus.create({
+      title: 'List functional areas',
+      id: TrilogyJira.listFunctionalAreasContextMenuId,
+      parentId: TrilogyJira.contextMenuId,
+      ...Handler.defaultContextMenuProps,
+    });
   }
 
   static determineUrl(menuItemId, detectedKey) {
@@ -45,6 +54,12 @@ class TrilogyJira {
 
   static findTicketGivenLegacy(detectedKey) {
     const jql = `"Legacy Issue Key[Short text]" ~ "${detectedKey}"`;
+    return `${TrilogyJira.url}/issues/?jql=${encodeURI(jql)}`;
+  }
+
+  static listFunctionalAreas(detectedKey) {
+    const projectKey = detectedKey.split('-')[0];
+    const jql = `project = ${projectKey} and type = "Functional Area"`;
     return `${TrilogyJira.url}/issues/?jql=${encodeURI(jql)}`;
   }
 }
